708b4baded5a616167d87943c964a5a93db0e987
runegranath <rune.granath@gmail.com>
2026-06-04 12:16:48 +0200

    ändrat från run till Tursos .execute

--- install.js
+++ install.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
 const express = require("express");
-const sqlite3 = require("sqlite3").verbose();
 
 // SQLite
 const { connect } = require("@libsql/client");
@@ -11,8 +10,7 @@ const db = connect({
 });
 
 // Skapa tabellen users
-db.serialize(() => {
-  db.run(`CREATE TABLE IF NOT EXISTS users (
+  db.execute(`CREATE TABLE IF NOT EXISTS users (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         email VARCHAR(255) NOT NULL UNIQUE,
         password VARCHAR(255) NOT NULL,
@@ -21,17 +19,16 @@ db.serialize(() => {
   console.log("Tabellen users skapad");
 
   // Skapa tabellen för menyer med id, år, veckonummer och publiceringsstatus, ska vara unik på år + veckonummer
-  db.run(`CREATE TABLE IF NOT EXISTS menus (
+  db.execute(`CREATE TABLE IF NOT EXISTS menus (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     year INTEGER NOT NULL,
     week_number INTEGER NOT NULL,
     UNIQUE(year, week_number)
 )`);
   console.log("Tabellen menus skapad");
-});
 
 // Maten, varje rad är en maträtt som kopplas till en vecka via menu_id och on delete cascade så att när en meny tas bort så tas alla maträtter för den veckan bort också
-db.run(`CREATE TABLE IF NOT EXISTS dishes (
+db.execute(`CREATE TABLE IF NOT EXISTS dishes (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     menu_id INTEGER NOT NULL,
     day_of_week VARCHAR(50) NOT NULL,
@@ -42,7 +39,7 @@ db.run(`CREATE TABLE IF NOT EXISTS dishes (
 )`);
 
 // Beställningar
-db.run(`CREATE TABLE IF NOT EXISTS orders (
+db.execute(`CREATE TABLE IF NOT EXISTS orders (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     dish_id INTEGER NOT NULL,
     customer_name VARCHAR(255) NOT NULL,
@@ -52,5 +49,3 @@ db.run(`CREATE TABLE IF NOT EXISTS orders (
     quantity INTEGER DEFAULT 1,
     FOREIGN KEY (dish_id) REFERENCES dishes(id)
 )`);
-
-db.close();
